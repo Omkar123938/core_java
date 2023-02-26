c2dcfb3651b4e53e6dcd53b3791a45a4a86553a8
Omkar123938 <omkargautam7308@gmail.com>
2023-02-26 13:47:56 +0530

    day1 code

---
+++ datavariable.java
@@ -0,0 +1,32 @@
+class datavariable {
+    public static void main(String a[]) {
+
+        // type variableName = value;
+
+        int myNum = 2;
+        byte by = 127;
+        short sh = 558;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = 8;
+        boolean b = true;
+        String name = "jhon";
+
+        // Declaring multiple variables
+        int x = 20, y = 30, z = 100;
+
+        System.out.println(myNum);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println("Hello " + name);
+        System.out.println(x + y + z);
+
+    }
+
+}
---
+++ hellow.java
@@ -0,0 +1,6 @@
+class hellow {
+    public static void main(String a[]) {
+        System.out.println("hellow world");
+        System.out.println(8 + 7);
+    }
+}
